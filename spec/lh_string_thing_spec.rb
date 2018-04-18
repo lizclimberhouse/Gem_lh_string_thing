056c88b3e26fc48db97da750aa68c545eb3e2caf
Liz House <lizehouse@gmail.com>
2018-04-18 10:56:56 -0600

    tests complete

--- spec/lh_string_thing_spec.rb
+++ spec/lh_string_thing_spec.rb
@@ -1,9 +1,27 @@
 RSpec.describe LhStringThing do
+  subject { LhStringThing }
+  let(:str) { 'My String' }
+
   it "has a version number" do
     expect(LhStringThing::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it 'reverse a string' do
+    actual = subject.reversify(str)
+    expected = 'gnirtS yM'
+    expect(actual).to eq(expected)
   end
+
+  it 'adds spaces' do
+    actual = subject.spacify(str, 1)
+    expected = 'M y   S t r i n g'
+    expect(actual).to eq(expected)
+  end
+  #   it "has a version number" do
+  #   expect(LhStringThing::VERSION).not_to be nil
+  # end
+
+  # it "does something useful" do
+  #   expect(false).to eq(true)
+  # end
 end
